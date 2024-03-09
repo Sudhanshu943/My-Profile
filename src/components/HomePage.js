8e9456862872096ca05c537445cbf3af273dfaad
Sudhanshu <sudhanshuthapa8@gmail.com>
2024-03-09 18:04:48 +0530

    contentpage page added and some changes are done in the privous files

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,47 +1,63 @@
-import React from 'react'
-import './HomePage'
+import React, { useState } from 'react';
+import { FaFacebookF, FaGithubSquare, FaInstagramSquare } from 'react-icons/fa';
+import ContentPage from './contentPage';
+import './HomePage.css'; // Import CSS file for styling
 
 function HomePage() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
-    <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary bg-color-black">
-     <div className="container-fluid">
-        <a className="navbar-brand" href="/">Navbar</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent"     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <div className="homePageBackground">
+      <nav className="navbar navbar-expand-lg bg-transparent">
+        <div className="container-fluid bg-transparent">
+          <a className="navbar-brand text-white" href="/">Sudhanshu Thapa</a>
+          <button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                 <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <a className="nav-link active text-white" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
-                  <a className="nav-link" href="/">Link</a>
+                <a className="nav-link text-white" href="/">Link</a>
               </li>
-                <li className="nav-item dropdown">
-                 <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                 Dropdown
-                    </a>
-                <ul className="dropdown-menu">
+              <li className="nav-item dropdown" onMouseEnter={() => setIsDropdownOpen(true)} onMouseLeave={() => setIsDropdownOpen(false)}>
+                <a className="nav-link dropdown-toggle text-white" href="/" role="button" id="navbarDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className={`dropdown-menu ${isDropdownOpen ? 'show' : ''}`} aria-labelledby="navbarDropdown">
                   <li><a className="dropdown-item" href="/">Action</a></li>
                   <li><a className="dropdown-item" href="/">Another action</a></li>
                   <li><hr className="dropdown-divider"/></li>
                   <li><a className="dropdown-item" href="/">Something else here</a></li>
                 </ul>
-                          </li>
-                      <li className="nav-item">
-                      <a className="nav-link disabled" href='/' aria-disabled="true">Disabled</a>
-                 </li>
-                  </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled text-white" href='/' aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
             <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+              <button className="btn btn-outline-success text-white" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      <ContentPage />
+      <div className="SocialLinks">
+        <h1>This is the links </h1>
+        <div className="linkContainer">
+          <ul className='linkBox'>
+              <a className='linkOne ' aria-current="page" href="/"><FaFacebookF /></a>
+              <a className='linkOne ' aria-current="page" href="/"><FaGithubSquare /></a>
+              <a className='linkOne ' aria-current="page" href="/"><FaInstagramSquare /></a>
+          </ul>
+        </div>
+      </div>
     </div>
-  </div>
-</nav>
-</>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
---
+++ src/components/contentPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ContentPage() {
+  return (
+    <div className="content">
+      <h1>Welcome to the Content Page</h1><h1>this</h1>
+
+      <p>This is where you can put any information you want to display.</p>
+      <p>Feel free to add text, images, links, or any other content you need.</p>
+    </div>
+  );
+}
+
+export default ContentPage
